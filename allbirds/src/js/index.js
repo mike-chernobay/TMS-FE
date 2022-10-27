63087d0b5c89c476b4939adb3d75ba9f00fd2023
mike-chernobay <mike_ch@tut.by>
2022-10-27 22:40:08 +0300

    mogo header and main info ready with adaptive

--- allbirds/src/js/index.js
+++ allbirds/src/js/index.js
@@ -1,11 +1,17 @@
-const menuBtn = document.querySelector(".menu-btn");
-let menuOpen = false;
-menuBtn.addEventListener("click", () => {
-  if (!menuOpen) {
-    menuBtn.classList.add("open");
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove("open");
-    menuOpen = false;
-  }
-});
+const menuBtn = document.querySelector('.menu-btn')
+let menuOpen = false
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open')
+        menuOpen = true
+    } else {
+        menuBtn.classList.remove('open')
+        menuOpen = false
+    }
+})
+
+window.addEventListener('load', () => hideLoader())
+
+function hideLoader() {
+    document.querySelector('.loader-screen').classList.add('hidden')
+}
